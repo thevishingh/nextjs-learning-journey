cb0d75f8ad77fa0ff26bb4712ccbaed2cdabf9de
Vishal Singh <singhvishal2977@gmail.com>
2026-05-05 23:48:53 +0530

    Just learn Few page designs and creating routes

--- nextjs-app/src/app/page.tsx
+++ nextjs-app/src/app/page.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <nav className='flex py-10 mx-auto container items-center justify-between'>
+        <h1 className='text-xl font-semibold'>Home</h1>
+        <div className='flex cursor-pointer gap-2'>
+          <h1>Performance</h1>
+          <h1>Reliability</h1>
+        </div>
+      </nav>
+    </div>
   )
 }
